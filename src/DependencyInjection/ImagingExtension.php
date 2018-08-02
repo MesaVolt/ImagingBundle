7fdc9ef75f08e680e7c6bd76aa9acb036b7b8485
Mickael GOETZ <contact@mickael-goetz.com>
2018-08-02 04:01:59 +0200

    rename DI extension to fix bundle registration in Symfony

--- src/DependencyInjection/ImagingExtension.php
+++ src/DependencyInjection/ImagingExtension.php
@@ -14,7 +14,7 @@ use Symfony\Component\HttpKernel\DependencyInjection\Extension;
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class ImageExtension extends Extension
+class ImagingExtension extends Extension
 {
     /**
      * {@inheritDoc}
